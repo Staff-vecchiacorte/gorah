695cfd46dc9f1ae8de82e776f6ff6c1c251a0405
User <user@example.com>
2025-05-19 11:03:19 +0200

    carpaccio

--- src/data/foodMenu.ts
+++ src/data/foodMenu.ts
@@ -11,11 +11,6 @@ export const foodMenu: MenuItem[] = [
     ]
   },
   {
-    name: 'Carpaccio Black Angus',
-    description: 'Su letto di rucola, scaglie di parmigiano e zest di limone 150g',
-    price: '12€'
-  },
-  {
     name: 'Boulletes di Stagione',
     description: 'Su fonduta di caciocavallo del Matese 3pz',
     price: '7€'
@@ -23,11 +18,11 @@ export const foodMenu: MenuItem[] = [
   {
     name: 'Tartare di Manzo',
     description: 'Con uovo in crosta e polvere di oliva caiazzana, condita con soia e olio 100g',
-    price: '8€'
+    price: '10€'
   },
   {
     name: 'Tartare di Salmone',
-    description: 'Con burrata, zest di arancia e sale Maldon 100g',
+    description: '',
     price: '10€'
   },
   {
